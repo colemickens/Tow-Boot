d3e911cd6d8974cbd2388e01534b25933db9d1ec
Cole Mickens <cole.mickens@gmail.com>
2022-01-28 02:19:07 -0800

    hacks on hacks WIP

--- boards/raspberryPi-aarch64/default.nix
+++ boards/raspberryPi-aarch64/default.nix
@@ -1,36 +1,52 @@
 { config, lib, pkgs, ... }:
 
+# TODO: redesign feedback
+# - I can't combine armv6l and aarch64 :/
+# - I really don't understand some choices
+
 let
   inherit (config.helpers)
     composeConfig
   ;
-  raspberryPi-3 = composeConfig {
+  raspberryPi = composeConfig {
     config = {
-      device.identifier = "raspberryPi-3";
-      Tow-Boot.defconfig = "rpi_3_defconfig";
+      device.identifier = "raspberryPi";
+      Tow-Boot.defconfig = "rpi_defconfig";
     };
   };
-  raspberryPi-4 = composeConfig {
+  raspberryPi-aarch64 = composeConfig {
     config = {
-      device.identifier = "raspberryPi-4";
-      Tow-Boot.defconfig = "rpi_4_defconfig";
+      device.identifier = "raspberryPi-aarch64";
+      Tow-Boot.defconfig = "rpi_arm64_defconfig";
     };
   };
 
   configTxt = pkgs.writeText "config.txt" ''
-    [pi3]
-    kernel=Tow-Boot.noenv.rpi3.bin
-
-    [pi4]
-    kernel=Tow-Boot.noenv.rpi4.bin
-    enable_gic=1
-    armstub=armstub8-gic.bin
-    disable_overscan=1
-
-    [all]
+    # these apply to all, but can be overridden by device specific sections
     arm_64bit=1
     enable_uart=1
     avoid_warnings=1
+
+    [pi0]
+    kernel=Tow-Boot.noenv.rpi.bin
+    arm_64bit=0
+
+    [pi02]
+    kernel=Tow-Boot.noenv.rpi_arm64.bin
+
+    [pi3]
+    kernel=Tow-Boot.noenv.rpi_arm64.bin
+
+    [pi4]
+    kernel=Tow-Boot.noenv.rpi_arm64.bin
+    enable_gic=1
+    armstub=armstub8-gic.bin
+    disable_overscan=1
+    arm_boost=1
+
+    # override all filters and apply to all boards
+    [all]
+    avoid_warnings
   '';
 in
 {
@@ -58,6 +74,13 @@ in
     patches = [
       ./0001-configs-rpi-allow-for-bigger-kernels.patch
       ./0001-Tow-Boot-rpi-Increase-malloc-pool-up-to-64MiB-env.patch
+      
+      # Remove when updating to 2022.01
+      # https://patchwork.ozlabs.org/project/uboot/list/?series=273129&archive=both&state=*
+      ./1-2-rpi-Update-the-Raspberry-Pi-doucmentation-URL.patch
+
+      # TODO: write this patch
+      ./0001-Tow-Boot-pass-uboot-version-in-kernel-params.patch
     ];
     outputs.firmware = lib.mkIf (config.device.identifier == "raspberryPi-aarch64") (
       pkgs.callPackage (
@@ -86,11 +109,13 @@ in
       cp -v u-boot.bin $out/binaries/Tow-Boot.$variant.bin
     '';
 
+    # TODO: why is there "outputs.firmware", "installerPhase" AND "populateCommands"... ?
+
     # The Raspberry Pi firmware expects a filesystem to be used.
     writeBinaryToFirmwarePartition = false;
 
     diskImage = {
-      partitioningScheme = "mbr";
+      partitioningScheme = "mbr"; # why? the rpi boots fine from gpt, we should use it?
     };
     firmwarePartition = {
       partitionType = "0C";
