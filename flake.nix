8b5b7bab88bb51ee0b9455f471c3f0d12a3a9a88
Cole Mickens <cole.mickens@gmail.com>
2023-01-08 19:31:14 -0800

    flake/nixos-integration: simplify for hostPlatform assumptions

--- flake.nix
+++ flake.nix
@@ -7,22 +7,22 @@
 
   outputs = inputs:
     let
-      nameValuePair = name: value: { inherit name value; };
-      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "riscv64-linux" ];
-      forAllSystems = genAttrs supportedSystems;
+      # nameValuePair = name: value: { inherit name value; };
+      # genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      # supportedSystems = [ "x86_64-linux" "aarch64-linux" "riscv64-linux" ];
+      # forAllSystems = genAttrs supportedSystems;
 
       evalConfig = import "${inputs.nixpkgs}/nixos/lib/eval-config.nix";
 
-      allDevices = builtins.filter
-        (d: builtins.pathExists (../../. + "/boards/${d}/default.nix"))
-        (builtins.attrNames (builtins.readDir ../../boards));
+      # allDevices = builtins.filter
+      #   (d: builtins.pathExists (../../. + "/boards/${d}/default.nix"))
+      #   (builtins.attrNames (builtins.readDir ../../boards));
 
       # keepEval = (eval: eval.config.device.inRelease);
       # evals = builtins.map (device: evalWith { inherit device; }) allDevices;
       # releasedDevices = builtins.filter keepEval evals;
 
-      evalTowBoot = { device, config, system }:
+      evalTowBoot = { device, config }:
         (evalConfig {
           baseModules = [
             ./modules
@@ -30,17 +30,17 @@
             "${inputs.nixpkgs}/nixos/modules/misc/nixpkgs.nix" ##???
             (./. + "/boards/${device}/default.nix")
           ];
-          inherit system;
           modules = [
             { inherit config; }
-            { system.automaticCross = true; }
           ];
           specialArgs = { inherit inputs; };
         });
     in
-    rec {
-      nixosModules = [
-        (import ./nixos/integration.nix { inherit evalTowBoot; })
-      ];
+    {
+      nixosModules = {
+        default = (import ./nixos/integration.nix {
+          inherit evalTowBoot;
+        });
+      };
     };
 }
