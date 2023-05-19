a1548b250bec73b4cf326a2b77328f8532e5a287
Cole Mickens <cole.mickens@gmail.com>
2023-05-19 01:05:33 -0600

    rock5 collabora wip, updates

--- boards/radxa-rock5b/blobs.nix
+++ boards/radxa-rock5b/blobs.nix
@@ -1,36 +1,39 @@
 { runCommandNoCC, fetchurl, xxd }:
 
 let
-  rev = "9840e87723eef7c41235b89af8c049c1bcd3d133";
-  bl31Ver = "v1.28";
+  rev = "d6aad64d4874b416f25669748a9ae5592642a453";
+  bl31Ver = "v1.34";
   ramVer = "v1.08";
 in
 {
-  BL31 =
-    fetchurl {
-      url = "https://github.com/radxa/rkbin/raw/${rev}/bin/rk35/rk3588_bl31_${bl31Ver}.elf";
-      sha256 = "sha256-mCx3v6musfwuf+HKFpYfQrk5RGjx3TZ1LeRwONEZVYs=";
-    };
+  zero = fetchurl {
+    url = "https://dl.radxa.com/rock5/sw/images/others/zero.img.gz";
+    hash = "sha256-RLUPLPZjaL8xCPpfHU2tkBr/Sv3dPZ7bkxmCN8K7ODo=";
+  };
+
+  BL31 = fetchurl {
+    url = "https://github.com/radxa/rkbin/raw/${rev}/bin/rk35/rk3588_bl31_${bl31Ver}.elf";
+    hash = "sha256-zr+A6o9wHcTIs8l3E4PhVu7Yti8vt2TFIXcAwiPplaA=";
+  };
 
   # Originally 60e3 16__; 0x16e360, 1500000
   # Changed to 0x1c200, 115200, 00c2 01__
   # Finding the offset: `grep '60 \?e3 \?16'`
-  ram_init =
-    runCommandNoCC "rk3588-patched-ram_init"
-      {
-        nativeBuildInputs = [
-          xxd
-        ];
-        ram_init = fetchurl {
-          url = "https://github.com/radxa/rkbin/raw/${rev}/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_${ramVer}.bin";
-          sha256 = "sha256-E4olArzqDOvlgYM3kAYLIdOaZ7YD2OdjPYUFvTmt6ns=";
-        };
-      } ''
-      cat $ram_init > $out
+  ram_init = runCommandNoCC "rk3588-patched-ram_init"
+    {
+      nativeBuildInputs = [
+        xxd
+      ];
+      ram_init = fetchurl {
+        url = "https://github.com/radxa/rkbin/raw/${rev}/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_${ramVer}.bin";
+        hash = "sha256-E4olArzqDOvlgYM3kAYLIdOaZ7YD2OdjPYUFvTmt6ns=";
+      };
+    } ''
+    cat $ram_init > $out
 
-      # xxd -r - $out <<EOF
-      # 0000e7c0: 110d 2b0d 0000 0000 1e0d 3808 00c2 0120
-      # EOF
-    ''
+    # xxd -r - $out <<EOF
+    # 0000e7c0: 110d 2b0d 0000 0000 1e0d 3808 00c2 0120
+    # EOF
+  ''
   ;
 }
--- boards/radxa-rock5b/default.nix
+++ boards/radxa-rock5b/default.nix
@@ -1,7 +1,7 @@
 /*
 
-https://github.com/radxa/u-boot/tree/stable-5.10-rock5
-https://github.com/radxa/rkbin
+  https://github.com/radxa/u-boot/tree/stable-5.10-rock5
+  https://github.com/radxa/rkbin
 
 */
 { config, lib, pkgs, ... }:
@@ -26,69 +26,85 @@ in
   };
 
   Tow-Boot = {
-    # defconfig = "rock-5b-rk3588_defconfig";
-    config = [
-      (helpers: with helpers; {
-        USB_GADGET_MANUFACTURER = freeform ''"Radxa"'';
-        DISABLE_CONSOLE = lib.mkForce no;
-        DM_ETH = yes;
-        PHY_REALTEK = lib.mkForce yes;
-        PHY_RK630 = yes;
-        DEBUG_UART = lib.mkForce yes;
-      })
-      (helpers: with helpers; {
-        EFI_LOADER = yes;
-        SYS_MMCSD_RAW_MODE_U_BOOT_USE_PARTITION = no;
-        # Offset for U-Boot proper is hardcoded in our builder
-        # 0x80000 / 512
-        MTD_BLK_U_BOOT_OFFS = freeform "0x400";
-        SPL_LIBDISK_SUPPORT = no;
-      })
-      # Not present in this older revision
-      (helpers: with helpers; {
-        AUTOBOOT_MENUKEY = lib.mkForce (option no);
-        AUTOBOOT_USE_MENUKEY = lib.mkForce (option no);
-        CMD_CLS = lib.mkForce (option no);
-        SYSINFO = lib.mkForce (option no);
-        SYSINFO_SMBIOS = lib.mkForce (option no);
-      })
-      # Environment is not supported with the BSP build
-      (helpers: with helpers; {
-        ENV_IS_NOWHERE = lib.mkForce yes;
-        ENV_ADDR = lib.mkForce no;
-        ENV_IS_IN_SPI_FLASH = lib.mkForce no;
-        ENV_OFFSET = lib.mkForce no;
-        ENV_SECT_SIZE = lib.mkForce no;
-      })
-      (helpers: with helpers; {
-        OPTEE_CLIENT = no;
-        ANDROID_BOOTLOADER = yes;
-        ANDROID_BOOT_IMAGE = yes;
-        ANDROID_AVB = no;
-        ANDROID_BOOT_IMAGE_HASH = no;
-        LIBAVB = no;
-        AVB_VERIFY = no;
-        CMD_AVB = no;
-        LOG = yes;
-        LOG_CONSOLE = yes;
-        LOG_MAX_LEVEL = freeform "7";
-      })
-    ];
+    defconfig = "rock5b-rk3588_defconfig";
+    # config = [
+    #   (helpers: with helpers; {
+    #     SYS_WHITE_ON_BLACK = lib.mkForce yes;
+    #     TPL_ENV_IS_NOWHERE = lib.mkForce yes;
+    #     SPL_SPI_SUPPORT = lib.mkForce yes;
+    #   })
+    # ];
+
+    # config = [
+    #   (helpers: with helpers; {
+    #     USB_GADGET_MANUFACTURER = freeform ''"Radxa"'';
+    #     DISABLE_CONSOLE = lib.mkForce no;
+    #     DM_ETH = yes;
+    #     PHY_REALTEK = lib.mkForce yes;
+    #     PHY_RK630 = yes;
+    #     DEBUG_UART = lib.mkForce yes;
+    #   })
+    #   (helpers: with helpers; {
+    #     EFI_LOADER = yes;
+    #     SYS_MMCSD_RAW_MODE_U_BOOT_USE_PARTITION = no;
+    #     # Offset for U-Boot proper is hardcoded in our builder
+    #     # 0x80000 / 512
+    #     MTD_BLK_U_BOOT_OFFS = freeform "0x400";
+    #     SPL_LIBDISK_SUPPORT = no;
+    #   })
+    #   # Not present in this older revision
+    #   (helpers: with helpers; {
+    #     AUTOBOOT_MENUKEY = lib.mkForce (option no);
+    #     AUTOBOOT_USE_MENUKEY = lib.mkForce (option no);
+    #     CMD_CLS = lib.mkForce (option no);
+    #     SYSINFO = lib.mkForce (option no);
+    #     SYSINFO_SMBIOS = lib.mkForce (option no);
+    #   })
+    #   # Environment is not supported with the BSP build
+    #   (helpers: with helpers; {
+    #     ENV_IS_NOWHERE = lib.mkForce yes;
+    #     ENV_ADDR = lib.mkForce no;
+    #     ENV_IS_IN_SPI_FLASH = lib.mkForce no;
+    #     ENV_OFFSET = lib.mkForce no;
+    #     ENV_SECT_SIZE = lib.mkForce no;
+    #   })
+    #   (helpers: with helpers; {
+    #     OPTEE_CLIENT = no;
+    #     ANDROID_BOOTLOADER = yes;
+    #     ANDROID_BOOT_IMAGE = yes;
+    #     ANDROID_AVB = no;
+    #     ANDROID_BOOT_IMAGE_HASH = no;
+    #     LIBAVB = no;
+    #     AVB_VERIFY = no;
+    #     CMD_AVB = no;
+    #     LOG = yes;
+    #     LOG_CONSOLE = yes;
+    #     LOG_MAX_LEVEL = freeform "7";
+    #   })
+    # ];
 
     uBootVersion = "2023.07-rc2";
-    src = pkgs.fetchFromGitHub {
-      owner = "u-boot";
-      repo = "u-boot";
-      rev = "v2023.07-rc2";
-      sha256 = "sha256-xxxLj7ex7cccvpkvSrwJo4hiHOT8BFAtM9uAZ1DezcY=";
-    };
-    # uBootVersion = "2017.09";
-    # src = pkgs.fetchFromGitHub {
-    #   owner = "radxa";
-    #   repo = "u-boot";
-    #   rev = "75b12f8295f1216d8f871a23fca37d4c990d508d"; # stable-5.10-rock5
-    #   sha256 = "sha256-gjDLj7ex7cccvpkvSrwJo4hiHOT8BFAtM9uAZ1DezcY=";
-    # };
+    src =
+      let
+        s1 = pkgs.fetchFromGitLab {
+          domain = "gitlab.collabora.com";
+          owner = "hardware-enablement/rockchip-3588";
+          repo = "u-boot";
+          # 2023.07-rc2-rock5b at 18-may-2023
+          rev = "72d8b9c88abf60b0fcae0ccd7bd1cebea7246702";
+          sha256 = "sha256-lxGM2MnsRTwVSBpU+yCxTuEQM30xPv4u07D2SS9N6p0=";
+        };
+        p = "$out/configs/rock5b-rk3588_defconfig";
+        s2 = pkgs.runCommand "fix-rk3588-ubootdefconfig" { } ''
+          set -x
+          set -eu
+          cp -r ${s1} $out
+          chmod -R +w $out
+          echo "CONFIG_VIDEO=y" >> ${p}
+          cat $out/configs/rock5b-rk3588_defconfig
+        '';
+      in
+      s2;
 
     # This is based on the Rockchip BSP
     useDefaultPatches = false;
@@ -96,39 +112,48 @@ in
     # Disable features causing trouble
     withLogo = false;
 
-    builder.additionalArguments = {
-      inherit BL31;
-    };
+    # builder.preBuild = ''
+    #   substituteInPlace arch/arm/mach-rockchip/decode_bl31.py \
+    #     --replace "/usr/bin/env python2" "${pkgs.buildPackages.python2}/bin/python2"
+    # '';
 
-    builder.preBuild = ''
-      substituteInPlace arch/arm/mach-rockchip/decode_bl31.py \
-        --replace "/usr/bin/env python2" "${pkgs.buildPackages.python2}/bin/python2"
-    '';
+    builder.additionalArguments = {
+      BL31 = BL31;
+      ROCKCHIP_TPL = ram_init;
+    };
 
     builder.installPhase = lib.mkMerge [
       # https://github.com/radxa/build/blob/428769f2ab689de27927af4bc8e7a9941677c366/mk-uboot.sh#L341-L347
       (lib.mkBefore ''
-        echo ':: Building specific outputs for the proprietary flavoured bits'
-        (PS4=" $ "; set -x
-        make $makeFlags "''${makeFlagsArray[@]}" spl/u-boot-spl.bin u-boot.dtb u-boot.itb
-        )
-        echo ':: Building proprietary flavoured idbloader.img'
-        (PS4=" $ "; set -x
-        echo "((((((((((((()))))))))))))"
+        # echo ':: Building specific outputs for the proprietary flavoured bits'
+        # (PS4=" $ "; set -x
+        # make $makeFlags "''${makeFlagsArray[@]}" spl/u-boot-spl.bin u-boot.dtb u-boot.itb
+        # )
+        # echo ':: Building proprietary flavoured idbloader.img'
+        # echo "((((((((((((()))))))))))))"
         ls -al spl/
-        echo "((((((((((((()))))))))))))"
+        # echo "((((((((((((()))))))))))))"
+        echo
+        echo
+        ls -al 
+        # echo "((((((((((((()))))))))))))"
         
-        tools/mkimage \
-          -n rk3588 \
-          -T "rksd" \
-          -d "${ram_init}:spl/u-boot-spl.bin" \
-          idbloader.img
-        )
+        # tools/mkimage \
+        #   -n rk3588 \
+        #   -T "rksd" \
+        #   -d "${ram_init}:spl/u-boot-spl.bin" \
+        #   idbloader.img
+
+        cp ${blobs.zero} $out/
+        cp u-boot-rockchip.bin $out/binaries/
+        cp u-boot-rockchip-spi.bin $out/binaries/
+        cp u-boot.itb $out/binaries/
+        cp idbloader.img $out/binaries/
       '')
     ];
 
     patches = [
-      ./patches/0001-BACKPORT-cmd-pxe-Increase-maximum-path-length.patch
+      #   ./patches/0001-BACKPORT-cmd-pxe-Increase-maximum-path-length.patch
       # ./patches/0001-rk3588_common-Disable-mtd-boot-target.patch
       # ./patches/0001-part_efi-Avoid-deluge-of-print-when-device-is-not-GP.patch
     ];
