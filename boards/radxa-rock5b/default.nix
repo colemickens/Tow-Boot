6c363e9bd5d4e1b927d38572277b2ebfd8507004
Cole Mickens <cole.mickens@gmail.com>
2023-08-28 01:19:27 -0500

    rock5b/evb1: hacky, config doesn't work for evb1 yet

--- boards/radxa-rock5b/default.nix
+++ boards/radxa-rock5b/default.nix
@@ -13,10 +13,10 @@ let
 in
 {
   device = {
-    manufacturer = "RadxAli";
-    name = "ROCK 5B";
-    identifier = "evb-rk3588";
-    productPageURL = "https://wiki.radxa.com/Rock5/hardware/5b";
+    manufacturer = "AliRock";
+    name = "H96 Max V58";
+    identifier = "rk3588-h96maxv58";
+    productPageURL = "https://wiki.radxa.com/Rock5/hardware/5b"; # TODO
   };
 
   hardware = {
@@ -26,91 +26,18 @@ in
 
   Tow-Boot = {
     buildUBoot = true;
-    defconfig = "evb-rk3588_defconfig";
-    config = [
-      (helpers: with helpers; {
-        SYS_WHITE_ON_BLACK = lib.mkForce yes;
-        TPL_ENV_IS_NOWHERE = lib.mkForce yes;
-        SPL_SPI_SUPPORT = lib.mkForce yes;
-      })
-    ];
-
-    # config = [
-    #   (helpers: with helpers; {
-    #     USB_GADGET_MANUFACTURER = freeform ''"Radxa"'';
-    #     DISABLE_CONSOLE = lib.mkForce no;
-    #     DM_ETH = yes;
-    #     PHY_REALTEK = lib.mkForce yes;
-    #     PHY_RK630 = yes;
-    #     DEBUG_UART = lib.mkForce yes;
-    #   })
-    #   (helpers: with helpers; {
-    #     EFI_LOADER = yes;
-    #     SYS_MMCSD_RAW_MODE_U_BOOT_USE_PARTITION = no;
-    #     # Offset for U-Boot proper is hardcoded in our builder
-    #     # 0x80000 / 512
-    #     MTD_BLK_U_BOOT_OFFS = freeform "0x400";
-    #     SPL_LIBDISK_SUPPORT = no;
-    #   })
-    #   # Not present in this older revision
-    #   (helpers: with helpers; {
-    #     AUTOBOOT_MENUKEY = lib.mkForce (option no);
-    #     AUTOBOOT_USE_MENUKEY = lib.mkForce (option no);
-    #     CMD_CLS = lib.mkForce (option no);
-    #     SYSINFO = lib.mkForce (option no);
-    #     SYSINFO_SMBIOS = lib.mkForce (option no);
-    #   })
-    #   # Environment is not supported with the BSP build
-    #   (helpers: with helpers; {
-    #     ENV_IS_NOWHERE = lib.mkForce yes;
-    #     ENV_ADDR = lib.mkForce no;
-    #     ENV_IS_IN_SPI_FLASH = lib.mkForce no;
-    #     ENV_OFFSET = lib.mkForce no;
-    #     ENV_SECT_SIZE = lib.mkForce no;
-    #   })
-    #   (helpers: with helpers; {
-    #     OPTEE_CLIENT = no;
-    #     ANDROID_BOOTLOADER = yes;
-    #     ANDROID_BOOT_IMAGE = yes;
-    #     ANDROID_AVB = no;
-    #     ANDROID_BOOT_IMAGE_HASH = no;
-    #     LIBAVB = no;
-    #     AVB_VERIFY = no;
-    #     CMD_AVB = no;
-    #     LOG = yes;
-    #     LOG_CONSOLE = yes;
-    #     LOG_MAX_LEVEL = freeform "7";
-    #   })
-    # ];
+    defconfig = "h96maxv58-rk3588_defconfig";
 
     uBootVersion = "2023.10-rc1";
     src =
       let
-        s1 = pkgs.fetchFromGitLab {
-          domain = "gitlab.collabora.com";
-          owner = "hardware-enablement/rockchip-3588";
+        s0 = pkgs.fetchFromGitHub {
+          owner = "colemickens";
           repo = "u-boot";
-          # https://gitlab.collabora.com/hardware-enablement/rockchip-3588/u-boot/-/commits/2023.10-rc1-rk3588/
-          # 13-aug-2023
-          rev = "46349e27812413f73197fc3eec460743940314de";
-          sha256 = "sha256-42fsXKlJZ7RTfZeN4RuVGHxK0zyyyyyfzRteU4BmGb0=";
+          rev = "80bc2c23726a52fe85c275d61e184d96bed66ebc";
+          hash = "sha256-r6mK11Z4DCtzHIlwiqY2WQilHrfsGdVv2bawstW2XQM=";
         };
-        p = "$out/configs/rock5b-rk3588_defconfig";
-        p2 = "$out/configs/evb-rk3588_defconfig";
-        s2 = pkgs.runCommand "fix-rk3588-ubootdefconfig" { } ''
-          set -x
-          set -eu
-          cp -r ${s1} $out
-          chmod -R +w $out
-          echo "CONFIG_VIDEO=y" >> "${p}"
-          echo "CONFIG_VIDEO=y" >> "${p2}"
-          echo "CONFIG_SPL_SPI_SUPPORT=y" >> "${p2}"
-          echo "CONFIG_SPL_SPI_LOAD=y" >> "${p2}"
-          echo "CONFIG_SYS_SPI_U_BOOT_OFFS=0x60000" >> "${p2}"
-        '';
-      in
-      s1;
-      # s2;
+        in s0;
 
     # Disable features causing trouble
     withLogo = false;
@@ -134,22 +61,22 @@ in
         # )
         # echo ':: Building proprietary flavoured idbloader.img'
         # echo "((((((((((((()))))))))))))"
-        ls -al spl/
-        # echo "((((((((((((()))))))))))))"
-        echo
-        echo
-        ls -al 
-        # echo "((((((((((((()))))))))))))"
+        # ls -al spl/
+        # # echo "((((((((((((()))))))))))))"
+        # echo
+        # echo
+        # ls -al 
+        # # echo "((((((((((((()))))))))))))"
         
-        # tools/mkimage \
-        #   -n rk3588 \
-        #   -T "rksd" \
-        #   -d "${blobs.ddrInit}:spl/u-boot-spl.bin" \
-        #   idbloader.img
+        # # tools/mkimage \
+        # #   -n rk3588 \
+        # #   -T "rksd" \
+        # #   -d "${blobs.ddrInit}:spl/u-boot-spl.bin" \
+        # #   idbloader.img
 
         cp ${blobs.zero} $out/zero.img.gz
         cp u-boot-rockchip.bin $out/binaries/
-        cp u-boot-rockchip-spi.bin $out/binaries/
+        cp u-boot-rockchip-spi.bin $out/binaries/ || true ### TODO lazy
         cp u-boot.itb $out/binaries/
         cp idbloader.img $out/binaries/
       '')
